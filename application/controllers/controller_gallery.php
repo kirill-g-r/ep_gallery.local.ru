a386735feca3813940373b211fa1b38955fbb2b6
gkg <kirillgr2@gmail.com>
2015-06-18 01:00:38 +0300

    Updates

--- application/controllers/controller_gallery.php
+++ application/controllers/controller_gallery.php
@@ -5,17 +5,8 @@ class Controller_Gallery extends Controller
 
     function action_index() {
 
-        if ( $this->userIsLogged() ) {
-
-            //header('Location:/charges');
-            $this->view->generate('', 'gallery_view.php');
-
-        } else {
-
-            $this->view->generate('', 'gallery_view.php');
-            //$this->view->generate('main_view.php', 'template_view.php');
-
-        }
+        $this->view->generate('', 'gallery_view.php');
+        //$this->view->generate('main_view.php', 'template_view.php');
 
     }
 
--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -5,17 +5,10 @@ class Controller_Main extends Controller
 				
 	function action_index() {
 				
-		if ( $this->userIsLogged() ) {
+		$this->view->generate('', 'main_view.php');
+		//$this->view->generate('main_view.php', 'template_view.php');
 			
-			//header('Location:/charges');
-			$this->view->generate('', 'main_view.php');
-		
-		} else {
-			
-			$this->view->generate('', 'main_view.php');
-			//$this->view->generate('main_view.php', 'template_view.php');
-			
-		}
+
 			
 	}
 		
