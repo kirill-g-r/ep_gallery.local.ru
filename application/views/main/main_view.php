86c7ffd01dbe95d2820a1dae3d29127dc9f3ae60
gkg <kirillgr2@gmail.com>
2015-06-25 01:09:30 +0300

    Updates

--- application/views/main/main_view.php
+++ application/views/main/main_view.php
@@ -1,87 +1,341 @@
+
 <!DOCTYPE HTML>
 <!--
-    Developed By
-	Kirill Goryunov, 2015
+    ELENA OPALEVA Official site
+    by KIRILLGORYUNOV.COM
 
-	KIRILLGORYUNOV.COM
 	email: info@KIRILLGORYUNOV.COM
-
 -->
 <html>
+	<head>
+		<title>Elena Opaleva | Official Site</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+    <style>
+        @font-face {
+            font-family: "Zapfino";
+            src: url(http://kirillgoryunov.com/fonts/Zapfino.otf) ;
+            font-weight: bold;
+
+
+        }
+        h1 { font-family: "Zapfino"; text-shadow: 0px 3px 0px #b2a98f,
+        0px 14px 10px rgba(0,0,0,0.15),
+        0px 24px 2px rgba(0,0,0,0.1),
+        0px 34px 30px rgba(0,0,0,0.1);}
+
+
+    </style>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								<h1><a href="index.html" id="logo">Elena Opaleva</a></h1>
+								<hr />
+								<p>ARTIST</p>
+							</header>
+							<footer>
+								<a href="#banner" class="button circled scrolly fa fa-arrow-circle-down fa-5x"></a>
+							</footer>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li><a href="#">Artworks</a></li>
+								<li><a href="left-sidebar.html">Artist</a>
+                                    <ul>
+                                        <li><a href="#">Biographi</a></li>
+                                        <li>
+                                            <a href="#">Media &hellip;</a>
+                                            <ul>
+                                                <li><a href="#">Photo</a></li>
+                                                <li><a href="#">Video</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Exhibitions</a></li>
+                                        <li><a href="#">Publishing</a></li>
+                                    </ul>
+
+                                </li>
+								<li><a href="right-sidebar.html">News</a></li>
+								<li><a href="no-sidebar.html">Articles</a></li>
+                                <li><a href="no-sidebar.html">Contacts</a></li>
+							</ul>
+						</nav>
+
+				</div>
+
+			<!-- Banner -->
+				<section id="banner">
+					<header>
+						<h2>Artworks</h2>
+<!--
+						<p>
+							A (free) responsive site template by <a href="http://html5up.net">HTML5 UP</a>.
+							Built on <strong>skel</strong> and released under the <a href="http://html5up.net/license">CCA</a> license.
+						</p>
+-->
+					</header>
+				</section>
+
+			<!-- Carousel -->
+				<section class="carousel">
+					<div class="reel">
+
+						<article>
+							<a href="#" class="image featured"><img src="images/1.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">#Натюрморт</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/3.JPG" alt="" /></a>
+							<header>
+								<h3><a href="#">#Композиция</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/10.JPG" alt="" /></a>
+							<header>
+								<h3><a href="#">#Пейзаж</a></h3>
+							</header>
+							<p>Non potest esse text, non potest potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/17.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">#Пейзаж</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/20.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">#Композиция</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">#Title</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">#Title</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">#Title</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">#Title</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">#Title</a></h3>
+							</header>
+							<p>Non potest esse text</p>
+						</article>
+
+					</div>
+				</section>
 
 
 
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+						<div class="row">
 
-<head>
-    <title>ELENA OPALEVA | Official Site</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <link rel="shortcut icon" href="http://kirillgoryunov.com/images/favicon_evo.ico" type="image/x-icon">
-    <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-    <link rel="stylesheet" href="http://kirillgoryunov.com/css/style_evo.css" />
-    <!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
-    <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+							<!-- Tweets -->
+								<section class="4u 12u(mobile)">
+									<header>
+                                        <h3>News</h3>
+                                        <br>
+										<h2 class="icon fa-twitter circled"><span class="label">Tweets</span></h2>
+									</header>
+									<ul class="divided">
+										<li>
+											<article class="tweet">
+												Добавлена новая картина в разделе натюрморт.
+												<span class="timestamp">40 minutes ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+                                                С 29 ноября по 12 декабря в картинной галерее пройдет выставка работв ...текст
+												<span class="timestamp">1 day ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+												Some text, some text, some text, some text, some text.
+												<span class="timestamp">3 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+                                                , some text, some text, some text, some text, some text, some text, some text.
+												<span class="timestamp">5 hours ago</span>
+											</article>
+										</li>
+									</ul>
+								</section>
 
-    <link href='http://fonts.googleapis.com/css?family=Niconne|Marck+Script|Alex+Brush|Calligraffitti|Tangerine|Great+Vibes|Pinyon+Script|Italianno' rel='stylesheet' type='text/css'>
-</head>
+                            <!-- Photos -->
+                            <section class="4u 12u(mobile)">
+                                <header>
+                                    <h3>Last Artworks</h3>
+                                    <br>
+                                    <h2 class="icon fa-camera circled"><span class="label">Photos</span></h2>
+                                </header>
+                                <div class="row 25%">
+                                    <div class="6u">
+                                        <a href="#" class="image fit"><img src="images/20.jpg" alt="" /></a>
+                                    </div>
+                                    <div class="6u$">
+                                        <a href="#" class="image fit"><img src="images/10.JPG" alt="" /></a>
+                                    </div>
+                                    <div class="6u">
+                                        <a href="#" class="image fit"><img src="images/17.jpg" alt="" /></a>
+                                    </div>
+                                    <div class="6u$">
+                                        <a href="#" class="image fit"><img src="images/1.jpg" alt="" /></a>
+                                    </div>
+                                    <div class="6u">
+                                        <a href="#" class="image fit"><img src="images/pic14.jpg" alt="" /></a>
+                                    </div>
+                                    <div class="6u$">
+                                        <a href="#" class="image fit"><img src="images/pic15.jpg" alt="" /></a>
+                                    </div>
+                                </div>
+                            </section>
 
-<style>
+							<!-- Posts -->
+								<section class="4u 12u(mobile)">
+									<header>
+                                        <h3>Articles</h3>
+                                        <br>
 
-    @font-face {
-        font-family: "Zapfino";
-        src: url(http://kirillgoryunov.com/fonts/Zapfino.otf) ;
-        font-weight: bold;
+										<h2 class="icon fa-file circled"><span class="label">Posts</span></h2>
+									</header>
+									<ul class="divided">
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Как правильно подобрать обрамление для картины</a></h3>
+												</header>
+												<span class="timestamp">3 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Some text, some text</a></h3>
+												</header>
+												<span class="timestamp">6 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Some text, some text</a></h3>
+												</header>
+												<span class="timestamp">Yesterday</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Some text, some text</a></h3>
+												</header>
+												<span class="timestamp">2 days ago</span>
+											</article>
+										</li>
+									</ul>
+								</section>
 
-    }
-    h2 { font-family: "Zapfino"}
+						</div>
+						<hr />
+						<div class="row">
+							<div class="12u">
 
-</style>
+								<!-- Contact -->
+									<section class="contact">
+										<header>
+											<h3>Contacts or some text</h3>
+										</header>
+										<p>Find on social networks ...and some text also</p>
+										<ul class="icons">
+											<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+											<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+											<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+											<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+											<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+											<li><a href="#" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+										</ul>
+									</section>
 
+								<!-- Copyright -->
+									<div class="copyright">
+										<ul class="menu">
+											<li>&copy; 2015 ELENA OPALEVA. All rights reserved.</li><li>Developed by <a href="http://kirillgoryunov.com">KIRILL GORYUNOV</a></li>
+										</ul>
+									</div>
 
+							</div>
 
-<body class="landing">
+						</div>
+					</div>
+				</div>
 
-<!-- Page Wrapper -->
-<div id="page-wrapper">
+		</div>
 
-    <!-- Header -->
-    <header id="header" class="alt">
-        <h1><a href="index.html">ELENA OPALEVA</a></h1>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.onvisible.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
 
-    </header>
-
-    <!-- Banner -->
-    <section id="banner">
-        <div class="inner">
-            <h2 style="font-size:400%">Elena Opaleva</h2>
-
-
-
-            <p>COMING SOON<br />
-
-            </p>
-
-            <div style="text-align: right; margin-right: 150px; margin-bottom: -100px;" >
-
-            <img class="raised" style="width: 200px; -webkit-transform: rotate(20deg); -moz-transform: rotate(-2deg); border: solid; " src="../../../images/20.jpg">
-
-            <img class="raised" style="width: 300px; -webkit-transform: rotate(5deg); -moz-transform: rotate(-2deg); border: solid; " src="../../../images/10.JPG">
-
-            <img class="raised" style="width: 250px; margin-bottom: 100px; -webkit-transform: rotate(-30deg); -moz-transform: rotate(-2deg); border: solid; " src="../../../images/17.jpg">
-
-                </div>
-        </div>
-
-    </section>
-
-    <!-- Scripts -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery.scrollex.min.js"></script>
-    <script src="js/jquery.scrolly.min.js"></script>
-    <script src="js/skel.min.js"></script>
-    <script src="js/init.js"></script>
-
-</body>
+	</body>
 </html>
---
+++ application/views/main/main_view_coming_soon.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML>
+<!--
+    Developed By
+	Kirill Goryunov, 2015
+
+	KIRILLGORYUNOV.COM
+	email: info@KIRILLGORYUNOV.COM
+
+-->
+<html>
+
+
+
+
+<head>
+    <title>ELENA OPALEVA | Official Site</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="shortcut icon" href="http://kirillgoryunov.com/images/favicon_evo.ico" type="image/x-icon">
+    <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="http://kirillgoryunov.com/css/style_evo.css" />
+    <!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+
+    <link href='http://fonts.googleapis.com/css?family=Niconne|Marck+Script|Alex+Brush|Calligraffitti|Tangerine|Great+Vibes|Pinyon+Script|Italianno' rel='stylesheet' type='text/css'>
+</head>
+
+<style>
+
+    @font-face {
+        font-family: "Zapfino";
+        src: url(http://kirillgoryunov.com/fonts/Zapfino.otf) ;
+        font-weight: bold;
+
+    }
+    h2 { font-family: "Zapfino"}
+
+</style>
+
+
+
+<body class="landing">
+
+<!-- Page Wrapper -->
+<div id="page-wrapper">
+
+    <!-- Header -->
+    <header id="header" class="alt">
+        <h1><a href="index.html">ELENA OPALEVA</a></h1>
+
+    </header>
+
+    <!-- Banner -->
+    <section id="banner">
+        <div class="inner">
+            <h2 style="font-size:400%">Elena Opaleva</h2>
+
+
+
+            <p>COMING SOON<br />
+
+            </p>
+
+            <div style="text-align: right; margin-right: 150px; margin-bottom: -100px;" >
+
+            <img class="raised" style="width: 200px; -webkit-transform: rotate(20deg); -moz-transform: rotate(-2deg); border: solid; " src="../../../images/20.jpg">
+
+            <img class="raised" style="width: 300px; -webkit-transform: rotate(5deg); -moz-transform: rotate(-2deg); border: solid; " src="../../../images/10.JPG">
+
+            <img class="raised" style="width: 250px; margin-bottom: 100px; -webkit-transform: rotate(-30deg); -moz-transform: rotate(-2deg); border: solid; " src="../../../images/17.jpg">
+
+                </div>
+        </div>
+
+    </section>
+
+    <!-- Scripts -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.scrollex.min.js"></script>
+    <script src="js/jquery.scrolly.min.js"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/init.js"></script>
+
+</body>
+</html>
